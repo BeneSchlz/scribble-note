b26bff6d44cbfa210180978f520f333bd6bdb5e7
BeneSchlz <123456789+odin@bschulz735@gmail.com>
2025-01-08 09:35:38 +0100

    Error Message for empty title

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,11 @@ import tkinter as tk
 def save_to_file():
     title = title_field.get()
     text = text_field.get("1.0", "end-1c")
+
+    if not title:
+        print("Error: Title is empty. Please provide a title.")
+        return
+
     filename = f"/Users/benedictschulz/vault_obsidian/{title}.md"
 
     try:
@@ -16,20 +21,20 @@ def save_to_file():
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("scribble")
-    root.geometry("500x200")
+    root.geometry("700x400")
 
     button = tk.Button(
         root,
         text = "Save",
         command=save_to_file
     )
-    button.pack()
+    button.place(x=600, y=350)
 
 
-    title_field = tk.Entry(root, width=50)
+    title_field = tk.Entry(root, width=500)
     title_field.pack(padx=10, pady=5)   
 
-    text_field = tk.Text(root, wrap="word", width=50, height=10)
+    text_field = tk.Text(root, wrap="word", width=500, height=10)
     text_field.pack(padx=10, pady=10)
 
     root.mainloop()
